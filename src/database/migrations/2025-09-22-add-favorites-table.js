764f4cf119996d5489053654447e75735811f2d2
Al Reem <alreemt@andrew.cmu.edu>
2025-09-23 07:52:13 +0000

    fixing some tabbing and lint issues - alreemt

--- src/database/migrations/2025-09-22-add-favorites-table.js
+++ src/database/migrations/2025-09-22-add-favorites-table.js
@@ -3,21 +3,22 @@
 const db = require('../database');
 
 module.exports = {
-  name: 'add-favorites-table',
-  timestamp: Date.now(),
-  method: async function () {
-    await db.createTable('favorites', {
-      id: { type: 'serial', primaryKey: true },
-      student_id: { type: 'int', notNull: true },
-      announcement_id: { type: 'int', notNull: true },
-      timestamp: { type: 'datetime', notNull: true, defaultTo: 'CURRENT_TIMESTAMP' },
-    });
+    name: 'add-favorites-table',
+    timestamp: Date.now(),
+    method: async function () {
+        await db.createTable('favorites', {
+            id: { type: 'serial', primaryKey: true },
+            student_id: { type: 'int', notNull: true },
+            announcement_id: { type: 'int', notNull: true },
+            timestamp: { type: 'datetime', notNull: true, defaultTo: 'CURRENT_TIMESTAMP' },
+	    });
 
-    // adding the indexes
-    await db.addIndex('favorites', ['student_id']);
-    await db.addIndex('favorites', ['announcement_id']);
+	    // adding the indexes
+	    await db.addIndex('favorites', ['student_id']);
+	    await db.addIndex('favorites', ['announcement_id']);
 
-    // preventing the same student favoriting the same announcement twice on it
-    await db.addUnique('favorites', ['student_id', 'announcement_id']);
-  },
+		// preventing the same student favoriting the same announcement twice on it
+	    await db.addUnique('favorites', ['student_id', 'announcement_id']);
+    },  
 };
+
