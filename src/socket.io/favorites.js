41543c06c66d0259fd939423be6fc1249717c55d
mhmansoo <mhmansoo@andrew.cmu.edu>
2025-10-04 20:31:25 +0000

    Syntax errors

--- src/socket.io/favorites.js
+++ src/socket.io/favorites.js
@@ -1,39 +1,38 @@
 'use strict';
 
-const Favorites = require('../api/favorites');
+const { add, remove } = require('../api/favorites');
 
 const FavoritesSockets = {};
 
 FavoritesSockets.toggle = function (socket, data, callback) {
 	(async () => {
-		const uid = socket.uid;
+		const { uid } = socket;
 		if (!uid) {
 			const err = new Error('Unauthorized');
 			err.status = 401;
 			throw err;
 		}
 
-		const tid = Number(data && data.tid);
+		const tid = Number(data?.tid);
 		if (!Number.isFinite(tid)) {
 			const err = new Error('Invalid tid');
 			err.status = 400;
 			throw err;
 		}
 
-		// Try to add; if "already favorited" then remove (toggle).
 		try {
-			await Favorites.add(uid, tid);
+			await add(uid, tid);
 			return { favorited: true, targetId: tid };
 		} catch (e) {
 			if (e && /already\s*favorited/i.test(e.message || '')) {
-				await Favorites.remove(uid, tid);
+				await remove(uid, tid);
 				return { favorited: false, targetId: tid };
 			}
 			throw e;
 		}
 	})()
-		.then((result) => callback && callback(null, result))
-		.catch((err) => callback && callback(err));
+		.then(result => callback && callback(null, result))
+		.catch(err => callback && callback(err));
 };
 
 module.exports = FavoritesSockets;
